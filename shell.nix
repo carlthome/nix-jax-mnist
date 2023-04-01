05fb0a69be6c5796541e76a868431e4a14e4785f
Carl Thomé <carlthome@gmail.com>
2023-04-02 01:09:07 +0200

    Add back pipzone

--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ pkgs.mkShell {
       ipython
       black
       mypy
+    ] ++ [
       tqdm
       jax
       jaxlib
@@ -13,4 +14,12 @@ pkgs.mkShell {
       tensorflow-datasets
     ]))
   ];
+  shellHook = ''
+    # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+    export PIP_PREFIX=$(pwd)/.pip
+    export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    unset SOURCE_DATE_EPOCH
+  '';
 }
