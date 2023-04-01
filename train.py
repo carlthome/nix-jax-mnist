804005bf86f920b83feb21c8ea6d3cccf8e390a4
Carl Thomé <carlthome@gmail.com>
2023-04-02 01:08:44 +0200

    Sum then average, and save data to working directory by default

--- train.py
+++ train.py
@@ -11,15 +11,15 @@ def predict(x, W, b):
     return activations
 
 
-def loss(y_true, y_pred):
+def cross_entropy(y_true, y_pred):
     eps = 1e-9
-    nll = -y_true * np.log(y_pred + eps)
+    nll = (-y_true * np.log(y_pred + eps)).sum(axis=-1)
     return nll
 
 
-def cost(W, b, x, y):
+def forward(W, b, x, y):
     y_pred = predict(x, W, b)
-    return loss(y, y_pred).sum()
+    return cross_entropy(y, y_pred).mean(axis=0)
 
 
 def preprocess(examples):
@@ -37,13 +37,12 @@ def preprocess(examples):
 
 
 if __name__ == "__main__":
-
     # Choose hyperparameters.
     learning_rate = 0.01
     batch_size = 32
-    epochs = 10
+    num_epochs = 10
 
-    datasets, info = load("mnist", with_info=True)
+    datasets, info = load("mnist", with_info=True, data_dir=".data")
     print(info)
     training_data = datasets["train"].shuffle(1024).batch(batch_size)
     test_data = datasets["test"].batch(batch_size)
@@ -59,36 +58,55 @@ if __name__ == "__main__":
     )
     b = np.zeros([num_classes])
 
-    for epoch in trange(epochs, desc="Training model.", unit="epoch"):
+    # Train for a number of epochs.
+    epochs = trange(num_epochs, desc="Epochs", unit="epoch")
+    for epoch in epochs:
 
-        # Train
+        # Train on training set.
+        training = tqdm(
+            training_data.as_numpy_iterator(),
+            desc=f"Training",
+            unit="minibatches",
+            total=len(training_data),
+            leave=False,
+        )
         num_examples = 0
         total = 0.0
-        for examples in tqdm(training_data.as_numpy_iterator(), desc=f"Training epoch {epoch}", unit='minibatches', position=1):
+        for examples in training:
             x, y = preprocess(examples)
 
-            l, grads = value_and_grad(cost, argnums=[0, 1])(W, b, x, y)
+            loss, grads = value_and_grad(forward, argnums=[0, 1])(W, b, x, y)
 
             dldw, dldb = grads
             W = W + learning_rate * -dldw
             b = b + learning_rate * -dldb
 
+            total += loss
             num_examples += len(x)
-            total += l
+            training.set_postfix(loss=total / num_examples, refresh=False)
+        average_training_loss = total / num_examples
 
-            #print(f"Loss: {total/num_examples}")
-
-        # Evaluate
+        # Evaluate on test set.
+        testing = tqdm(
+            test_data.as_numpy_iterator(),
+            desc=f"Evaluating",
+            unit="minibatches",
+            total=len(test_data),
+            leave=False,
+        )
         num_examples = 0
         total = 0
-        for examples in tqdm(test_data.as_numpy_iterator(), desc=f"Evaluating epoch {epoch}", unit="minibatches", position=1):
+        for examples in testing:
             x, y = preprocess(examples)
             y_pred = predict(x, W, b)
 
-            total += np.count_nonzero(y.argmax(-1) == y_pred.argmax(-1))
+            accuracy = np.count_nonzero(y.argmax(-1) == y_pred.argmax(-1))
+
+            total += accuracy
             num_examples += len(x)
+            testing.set_postfix(loss=total / num_examples, refresh=False)
+        test_accuracy = total / num_examples
 
-        average_training_loss = total / num_examples
-        average_test_accuracy = total / num_examples
-
-        print(f"Epoch: {epoch}, Training Loss: {average_training_loss}, Test accuracy: {average_test_accuracy*100}%")
+        epochs.set_postfix(
+            epoch=epoch, average_loss=average_training_loss, accuracy=test_accuracy
+        )
